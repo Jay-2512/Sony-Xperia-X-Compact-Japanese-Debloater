01740752db062d2415cc6a5cd4caa882673db754
Jayasurya J <63704047+Jay-2512@users.noreply.github.com>
2021-01-12 12:46:48 +0530

    Update xperia_xc_debloater.bat

--- Windows/xperia_xc_debloater.bat
+++ Windows/xperia_xc_debloater.bat
@@ -1,7 +1,7 @@
 echo --Xperia SO-02J Debolater--
 
 echo loading...
-sleep 2
+timeout 2
 
 clear
 
@@ -12,238 +12,238 @@ echo ---------------------------
 
 echo Debloat initiating.
 
-sleep 2
+timeout 2
 
 
 echo Removing Amazon Kindle
 adb shell pm uninstall --user 0 com.amazon.kindle
-sleep 2
+timeout 2
 
 echo Removing lawson
 adb shell pm uninstall --user 0 jp.co.lawson.activity
-sleep 2
+timeout 2
 
 echo Removing McDonalds
 adb shell pm uninstall --user 0 jp.co.mcdonalds.android
-sleep 2
+timeout 2
 
 echo Removing Docomo App 
 adb shell pm uninstall --user 0 com.nttdocomo.android.mediaplayer
-sleep 2
+timeout 2
 
 echo Removing FeLiCa App
 adb shell pm uninstall --user 0 com.felicanetworks.mfc
-sleep 2
+timeout 2
 
 echo Removing MobileSelect
 adb shell pm uninstall --user 0 com.mobileselect.somcprein
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.voiceeditor
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.remotelock
-sleep 2
+timeout 2
 adb shell pm uninstall --user 0 com.felicanetworks.mfm.main
-sleep 2
+timeout 2
 adb shell pm uninstall --user 0 com.felicanetworks.mfm
-sleep 2
+timeout 2
 adb shell pm uninstall --user 0 com.felicanetworks.mfs
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.osaifu.tsmproxy
-sleep 2
+timeout 2
 adb shell pm uninstall --user 0 com.felicanetworks.mfw.a.main
-sleep 2
+timeout 2
 adb shell pm uninstall --user 0 com.felicanetworks.mfw.a.boot
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.devicehelp
-sleep 2
+timeout 2
 adb shell pm uninstall --user 0 com.android.dialer
-sleep 2
+timeout 2
 adb shell pm uninstall --user 0 com.sony.drbd.reader.other.jp
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.schedulememo
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.screenlockservice
-sleep 2
+timeout 2
 
 echo Removing Keyboard Skin
 adb shell pm uninstall --user 0 com.sonymobile.pobox.skin.standard
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.phonemotion
-sleep 2
+timeout 2
 
 echo Removing TV Digital 
 adb shell pm uninstall --user 0 jp.co.fsi.fs1seg
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.tapandpay
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.toruca
-sleep 2
+timeout 2
 
 echo Removing Keyboard Skin
 adb shell pm uninstall --user 0 com.sonymobile.pobox.skin.wood
-sleep 2
+timeout 2
 
 echo Removing Chineese Text Input
 adb shell pm uninstall --user 0 com.sonyericsson.textinput.chinese
-sleep 2
+timeout 2
 
 echo Removing Keyboard Japaneese
 adb shell pm uninstall --user 0 com.sonymobile.pobox
-sleep 2
+timeout 2
 
 echo Removing Sony package
 adb shell pm uninstall --user 0 com.sonymobile.entrance
-sleep 2
+timeout 2
 
 echo Removing FileManager
 adb shell pm uninstall --user 0 com.mobisystems.fileman
-sleep 2
+timeout 2
 
 echo Removing Xperia Lounge Japan
 adb shell pm uninstall --user 0 com.sonyericsson.androidapp.sehome
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.dpoint
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 jp.co.nttdocomo.dbook
-sleep 2
+timeout 2
 
 echo Removing Sony Bloat
 adb shell pm uninstall --user 0 com.sonymobile.deqp
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.store
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.applicationmanager
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.accountauthenticator
 
-sleep 2
+timeout 2
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.sdcardbackup
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.dhome
-sleep 2
+timeout 2
 
 echo Removing NextBit App
 adb shell pm uninstall --user 0 com.nextbit.app
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.cloudset
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 jp.co.nttdocomo.lcsapp
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 jp.co.nttdocomo.lcsappsub
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 jp.co.nttdocomo.carriermail
-sleep 2
+timeout 2
 
 echo Removing Contacts
 adb shell pm uninstall --user 0 com.android.contacts
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.docomoset
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.idmanager
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.dmenu2
-sleep 2
+timeout 2
 
 echo Removing Japanese App
 adb shell pm uninstall --user 0 jp.id_credit_sp.android
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.iconcier
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.iconcier_contents
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 jp.co.nttdocomo.anshinmode
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.mascot
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.databackup
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.cloudstorageservice
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.dcmvoicerecognition
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.photocollection
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.moneyrecord
-sleep 2
+timeout 2
 
 echo Removing Japanese App
 adb shell pm uninstall --user 0 jp.dmapnavi.navi02
-sleep 2
+timeout 2
 
 echo Removing Japanese App
 adb shell pm uninstall --user 0 com.rsupport.rs.activity.ntt
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.ictrw
-sleep 2
+timeout 2
 
 echo Removing Docomo App
 adb shell pm uninstall --user 0 com.nttdocomo.android.wipe
-sleep 2
+timeout 2
 
 
 echo Debloating Complete.
